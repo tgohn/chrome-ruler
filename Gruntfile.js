64f97597c3d9cb780fcadebb6234f374bad87c5a
John Nguyen <john@zopim.com>
2014-03-09 10:25:18 +0000

    Fix Grunt watch task again: wrong file selectors

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		watch: {
 			'chrome-ruler': {
-				files: ['src/chrome-ruler.js', 'src/chrome-ruler/**/*.{js.jsx}'],
+				files: ['src/chrome-ruler.js', 'src/chrome-ruler/**/*.{js,jsx}'],
 				tasks: ['exec:chrome-ruler'],
 				options: {
 					interupt: true,
