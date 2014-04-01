7d4d6e6b3d1692d747be0c2391c1918e83cdee77
John Nguyen <nguy0052@gmail.com>
2014-04-02 02:46:40 +0800

    Move background.js to /src

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -3,7 +3,7 @@
 
 var script_srcs = ['dist/chrome-ruler.js'];
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function(/*tab*/) {
 	script_srcs.forEach(function executeScript(src) {
 		chrome.tabs.executeScript(null, {
 			'file': src
