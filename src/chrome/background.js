fa0ccc5cf91871834d99b8c6a60292f50ac43993
John Nguyen <nguy0052@gmail.com>
2014-04-12 18:48:18 +0800

    Fix wrong location for minified script

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -1,7 +1,7 @@
 /* globals chrome */
 // Called when the user clicks on the browser action.
 
-var script_srcs = ['dist/chrome-ruler.js'];
+var script_srcs = ['dist/chrome-ruler.min.js'];
 
 chrome.browserAction.onClicked.addListener(function(/*tab*/) {
 	script_srcs.forEach(function executeScript(src) {
